152bc47c6738600ac8e727beb960a3352f2ebd7a
DiegoPerdomo0805 <100246718+DiegoPerdomo0805@users.noreply.github.com>
2022-03-16 21:44:19 -0600

    generate.sql
    Documento para generar las tablas acerca de los diferentes títulos para ver de la base de datos

---
+++ generate.sql
@@ -0,0 +1,69 @@
+CREATE table TITULOS (
+	ID varchar(6) primary key not null,
+    NOMBRE varchar(40) not null,
+    TIPO varchar(1) not null
+);
+
+CREATE table generos (
+	id_genero varchar(3) primary key not null,
+    NOMBRE varchar(12) not null
+);
+
+CREATE table actor (
+	id_actor varchar(4) primary key not null,
+    NOMBRE varchar(30) not null
+);
+
+CREATE table director (
+	id_director varchar(4) primary key not null,
+    NOMBRE varchar(30) not null
+);
+
+CREATE table peliculas (
+	id varchar(6) not null,
+    duracion int not null,
+    foreign key (id) references titulos(id),
+    primary key (id)
+);
+
+CREATE table series (
+	id varchar(6) not null,
+	temporadas int not null,
+    episodios int not null,
+    foreign key (id) references titulos(id),
+    primary key (id)
+);
+
+create table titulo_actores (
+	id varchar(6) not null,
+	estelar1 varchar(4) not null,
+	estelar2 varchar(4),
+	estelar3 varchar(4),
+    foreign key (id) references titulos(id),
+    foreign key (estelar1) references actor(id_actor),
+    foreign key (estelar2) references actor(id_actor),
+    foreign key (estelar3) references actor(id_actor),
+    primary key (id)
+);
+
+create table titulo_details (
+	id varchar(6) not null,
+	genero1 varchar(4) not null,
+	genero2 varchar(4),
+	genero3 varchar(4),
+    foreign key (id) references titulos(id),
+    foreign key (genero1) references generos(id_genero),
+    foreign key (genero2) references generos(id_genero),
+    foreign key (genero3) references generos(id_genero),
+    primary key (id)
+);
+
+create table titulo_director (
+	id varchar(6) not null,
+	director varchar(4) not null,
+    foreign key (id) references titulos(id),
+    foreign key (director) references director(id_director),
+    primary key (id)
+);
+
+
