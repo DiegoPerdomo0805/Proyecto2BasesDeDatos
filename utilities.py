1f7dec81de76773ba9729f6c5d1c7d4dc3bdd06f
DiegoP0805 <60204933+DiegoP0805@users.noreply.github.com>
2022-04-04 20:02:17 -0600

    Create utilities.py

---
+++ utilities.py
@@ -0,0 +1,15 @@
+from hashlib import md5
+
+#funcion de encriptado usando md5
+def encryption(psswrd):
+    cntr = psswrd
+    c_e = md5()
+    c_e.update(cntr.encode())
+    return c_e.digest()
+
+def passlen(psswrd):
+    if len(psswrd)>7 and len(psswrd)<50:
+        return True
+    else:
+        return False
+
