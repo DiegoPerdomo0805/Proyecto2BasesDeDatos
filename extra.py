51810d20b471b82bcc9ecd76d2d6e8797e964a0c
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-05-11 20:45:27 -0600

    cambio

--- extra.py
+++ extra.py
@@ -1,23 +1,23 @@
-import utilities, conexion, player
+import utilities
+import conexion
+import player
+
 
 def prog():
     print("Contenido en progreso")
 
-    print("\nEscoja un contenido para ver de la lista de favoritos")
-
-    sql = ("SELECT t.nombre from titulos t where t.id in (SELECT f.id_titulo from favoritos f where f.perfil like %s);")
+    sql = ("SELECT t.nombre from titulos t where t.id in (SELECT f.id_titulo from viendo f where f.perfil like %s);")
     # RECORDA SIEMPRE PONER LA COMA PARA QUE NO TRUENE
     args = (utilities.getProfile(),)
     nombres = conexion.executeQuery(sql, args, True)
 
-    sql = ("SELECT f.id_titulo from favoritos f where f.perfil like %s;")
+    sql = ("SELECT f.id_titulo from viendo f where f.perfil like %s;")
     args = (utilities.getProfile(),)  # RECORDA SIEMPRE PO
     ids = conexion.executeQuery(sql, args, True)
 
     choice = utilities.menu3(ids, nombres)
     print(choice)
 
-    player.videoPlayer(choice)
 
 def finalizado():
     print("Contenido finalizado")
@@ -38,6 +38,7 @@ def finalizado():
 
     player.videoPlayer(choice)
 
+
 def sugerencia():
     print("Contenido sugerido")
 
@@ -56,4 +57,3 @@ def sugerencia():
     print(choice)
 
     player.videoPlayer(choice)
-    
