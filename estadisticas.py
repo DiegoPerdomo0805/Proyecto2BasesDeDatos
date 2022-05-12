f579c89f4f7fece57f95c3e9de57d7faf1b39bc2
DiegoP0805 <60204933+DiegoP0805@users.noreply.github.com>
2022-05-11 20:02:48 -0600

    stat1

--- estadisticas.py
+++ estadisticas.py
@@ -34,11 +34,24 @@ def estadisticas():
             print("El valor debe ser una de las opciones dadas")
 
 def opcion1():
-    print("jaja")
-    
-    # sql = ("SELECT nombre from nombre where nombre = %s;")
-    # args = (userData,)#RECORDA SIEMPRE PONER LA COMA PARA QUE NO TRUENE
-    # results = conexion.executeQuery(sql, args, True) 
+    print("\nPelículas")
+    sql = ("SELECT (select g.nombre from generos g where g.id_genero = td.genero) as genero, sum(p.duracion * wa.times_watched) as minutos from titulo_details td inner join peliculas p on p.id = td.id inner join watch_again wa on wa.id_titulo = p.id group by genero order by minutos desc limit 10;")
+    args = ("",)
+    results = conexion.executeQuery(sql, args, True) 
+    i = 1
+    for e in results:
+      print(i, " . ", e)
+      i = i + 1
+
+    print("\nSeries")
+    sql = ("SELECT (select g.nombre from generos g where g.id_genero = td.genero) as genero, sum(s.episodios * 40 * wa.times_watched) as minutos from titulo_details td inner join series s on s.id = td.id inner join watch_again wa on wa.id_titulo = s.id group by genero order by minutos desc limit 10;")
+    args = ("",)#RECORDA SIEMPRE PONER LA COMA PARA QUE NO TRUENE
+    results = conexion.executeQuery(sql, args, True)
+    i = 1
+    for e in results:
+      print(i, " . ", e)
+      i = i + 1 
+
 
     #MOMO: Tambien como antes, solo imprimi y aqui lo que toque hacer
 
