0177c453b33ce2bc33f892ea5bacc8450f7a9e1e
DiegoP0805 <60204933+DiegoP0805@users.noreply.github.com>
2022-06-01 22:12:22 -0600

    conexion al servidor en linea

--- conexion.py
+++ conexion.py
@@ -1,6 +1,7 @@
 import psycopg2
 
-con=psycopg2.connect(user="postgres", password="NYARLATHOTEP", host="localhost", port="5432", database="proyecto2")
+####con=psycopg2.connect(user="postgres", password="NYARLATHOTEP", host="localhost", port="5432", database="proyecto2")
+con=psycopg2.connect(user="jose", password="AVNS_jSwbXXReHJ_sRzV", host="db-postgresql-nyc3-01635-do-user-11158832-0.b.db.ondigitalocean.com", port="25060", database="joseProyecto")
 
 con.autocommit = True
 cursor = con.cursor()
